55457f58e37265e1af217eced7027f73783339b8
Alex Rusin <alex@sunriseintegration.com>
2020-08-13 14:55:45 -0700

    scaffolding and routing

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const AboutPage = () => (
+    <>
+        <h1>Welcome About Page</h1>
+        <p>
+            Esse commodo aute cupidatat esse ad ex cillum id. Laboris ipsum sunt sunt ea eiusmod ipsum irure eu et nisi. Eu esse laborum tempor proident culpa nisi tempor adipisicing commodo minim. Esse occaecat ullamco laboris ut adipisicing nulla adipisicing. Do sunt dolor nisi consectetur officia.
+        </p>
+        <p>
+            Quis excepteur est sint est nostrud duis anim et tempor tempor id. Ullamco minim proident nulla deserunt. Irure reprehenderit cillum Lorem laboris ex veniam laborum deserunt. Qui magna veniam voluptate reprehenderit voluptate nulla proident do elit ipsum. Id esse exercitation in elit ea qui sit elit ea aliqua tempor. Ut eiusmod sit proident laborum elit incididunt cupidatat aliquip cupidatat amet incididunt cupidatat.
+        </p>
+        <p>
+            Laboris nisi sunt sunt anim ut ex Lorem adipisicing eu ullamco et. Deserunt incididunt cupidatat duis enim aliqua aliqua excepteur ullamco. In elit est in sunt amet ipsum excepteur veniam do sunt est. Voluptate pariatur excepteur qui consequat qui sit et minim ullamco incididunt cillum esse esse non.
+        </p>
+    </>
+);
+    
+
+
+export default AboutPage;
