314b7fdfe6571faa31d7dab120860267282b42dd
Alex Rusin <alex@sunriseintegration.com>
2020-08-14 08:50:52 -0700

    blog pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch
 } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import './App.css';
 import AboutPage from './pages/AboutPage';
-import ArticlesList from './pages/ArticlesList';
+import ArticlesListPage from './pages/ArticlesListPage';
 import ArticlePage from './pages/ArticlePage';
 import NavBar from './NavBar';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
@@ -16,10 +18,13 @@ function App() {
       <div className="App">
         <NavBar />
         <div id="page-body">
+          <Switch>
             <Route path="/" component={HomePage} exact />
             <Route path="/about" component={AboutPage} />
-            <Route path="/articles" component={ArticlesList} />
-            <Route path="/article" component={ArticlePage} />
+            <Route path="/articles" component={ArticlesListPage} />
+            <Route path="/article/:name" component={ArticlePage} />
+            <Route component={NotFoundPage} />
+          </Switch>
         </div>
       </div>
     </Router>
