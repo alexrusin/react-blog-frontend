2c97e13bcc9d6037f477eb1c17d84a421a8b2542
Alex Rusin <alex@sunriseintegration.com>
2020-08-16 08:45:54 -0700

    inital commit

---
+++ src/components/AddCommentForm.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+const AddCommentForm = ({ articleName, setArticleInfo}) => {
+    const [username, setUsername] = useState('');
+    const [commentText, setCommentText] = useState('');
+
+    const addComment = async() => {
+        const result = await fetch(`/api/articles/${articleName}/add-comment`, {
+            method: 'post',
+            body: JSON.stringify({username, text: commentText }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const body = await result.json();
+        setArticleInfo(body);
+        setUsername('');
+        setCommentText('');
+    }
+
+    return (
+    <div id="add-comment-form">
+        <h3>Add Comment</h3>
+        <label>
+            Name:
+            <input type="text"
+            onChange={(event) => setUsername(event.target.value)} 
+            value={username} />
+        </label>
+        <label>
+            Comment: 
+            <textarea rows="4" cols="50"
+            onChange={(event) => setCommentText(event.target.value)} 
+            value={commentText} />
+        </label>
+        <button onClick={() => addComment()}>Add Comment</button>
+    </div>
+    );
+}
+
+export default AddCommentForm;
